cd3b61c3be7ee5ddf46904c2d3dbdf391e5c5064
yasaswini-m <139933998+yasaswini-m@users.noreply.github.com>
2023-10-31 19:39:04 -0700

    Update Code_smells_example.py

--- Code_smells_example.py
+++ Code_smells_example.py
@@ -1,9 +1,9 @@
-def discount_multiplier(price):
+def factor(price):
     return 0.9 if price > 100 else 0.95
 
 def calculate_total_price(items):
     total = 0
     for item in items:
         if item.quantity > 0:
-            total += item.price * discount_multiplier(item.price)
+            total += item.price * factor(item.price)
     return total

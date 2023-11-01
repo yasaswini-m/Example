571bdff8d6db202e37e27d5df0d744786b956588
yasaswini-m <139933998+yasaswini-m@users.noreply.github.com>
2023-10-31 19:28:31 -0700

    Update Code_smells_example.py
    cognitive change

--- Code_smells_example.py
+++ Code_smells_example.py
@@ -1,10 +1,9 @@
-#Code Smells Example for long method
+def discount_multiplier(price):
+    return 0.9 if price > 100 else 0.95
+
 def calculate_total_price(items):
     total = 0
     for item in items:
         if item.quantity > 0:
-            if item.price > 100:
-                total += item.price * 0.9
-            else:
-                total += item.price * 0.95
+            total += item.price * discount_multiplier(item.price)
     return total
